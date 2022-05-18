ed48d4a5e9350c425a9e188ba90e234a039631f9
Denzel <dbartolaba123@gmail.com>
2022-05-18 16:26:51 -0700

    created the logic for the signIn/signOut/signUp

--- src/hooks/user.jsx
+++ src/hooks/user.jsx
@@ -1,11 +1,38 @@
+import { context } from 'msw';
 import { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../context/UserContext';
 import { signInUser, signOutUser, signUpUser } from '../services/users';
 
-const signUp = async (email, password) => {
-  try {
-    const user = await signUpUser(email, password);
-    
-  } catch (error) {
-    
+
+export const useAuth = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new error('useAuth must be used within a provider');
   }
-}
+
+
+  const { user, setUser } = context;
+
+  const signUp = async (email, password) => {
+    try {
+      const user = await signUpUser(email, password);
+      setUser(user)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const signIn = async (email, password) => {
+    try {
+      const user = await signInUser(email, password);
+      setUser(user)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const signOut = async () => {
+    await signOutUser();
+    setUser({});
+  }
+};
+
