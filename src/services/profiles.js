7798018244eb7dada90438a7dc14771a75780c67
Denzel <dbartolaba123@gmail.com>
2022-05-20 14:17:22 -0700

    created the create/delete function for profiles in supabase

--- src/services/profiles.js
+++ src/services/profiles.js
@@ -1,3 +1,4 @@
+import { parse } from 'dotenv';
 import { client, parseData } from './client';
 
 export async function getProfile(user_id) {
@@ -18,3 +19,20 @@ export async function updateProfile({ name, email, bio, dob }) {
   return parseData(request);
 }
 
+export async function createProfile({ name, email, bio, dob }) {
+  const request = await client
+    .from('profiles')
+    .insert({ name, bio, dob })
+    .match({ email })
+    .single();
+  return parseData(request);
+}
+
+export async function deleteProfile(email) {
+  const request = await client
+    .from('profiles')
+    .delete()
+    .match({ email });
+  return parseData(request);
+
+}
