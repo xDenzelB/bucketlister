2072b35d32c9425becfaa3811b309035369db882
Denzel <dbartolaba123@gmail.com>
2022-05-20 14:22:52 -0700

    set the get profile in user context

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { getUser } from "../services/users";
+import { getProfile } from "../services/profiles";
 
 export const UserContext = createContext();
 
@@ -7,9 +8,28 @@ export const UserProvider = ({ children }) => {
   const currentUser = getUser();
 
   const [user, setUser] = useState(currentUser ? { id: currentUser.id, email: currentUser.email } : {});
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    const loadingProfile = async () => {
+      setIsLoading(false);
 
-  return <UserContext.Provider value={{ user, setUser }}>
+    try {
+      if (!user) return setProfile();
+
+      const profile = await getProfile(user.id);
+      setProfile(profile);
+    } catch (error) {
+      setProfile(null);
+    } finally {
+      setIsLoading(true);
+    }
+    }
+    loadingProfile();
+  }, [user]);
+
+  return <UserContext.Provider value={{ user, setUser, profile, isLoaded, setProfile }}>
   {children}
 </UserContext.Provider>
 }
