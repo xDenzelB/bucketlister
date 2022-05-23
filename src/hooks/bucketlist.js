a05cf50a1fc43c32dfe4697078d31304844f6393
Denzel <dbartolaba123@gmail.com>
2022-05-23 11:51:29 -0700

    created the custom hooks for creating, updating, and deleting activities

--- src/hooks/bucketlist.js
+++ src/hooks/bucketlist.js
@@ -25,5 +25,89 @@ export function useBucketList() {
       }
     }
     load()
-  }, [])
+  }, []);
+
+  async function add(activity) {
+    try {
+      const payload = await createBucketListActivity(activity);
+      dispatch({ type: 'create', payload });
+      return payload
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return { activity, add };
+}
+
+export function useBucketListCount() {
+  const context = useContext(BucketListContext);
+  if (context === undefined) {
+    throw new Error(
+      'useBucketListcount must be used within a BucketListcontext'
+    );
+  }
+  return context.activity?.length;
+}
+
+export function useActivity(id) {
+  const context = useContext(BucketListContext);
+  const { profile } = useUser();
+
+  if (context === undefined) {
+    throw new Error('useActivity must be used within a BucketlistContext');
+
+  }
+
+  const { activity, dispatch } = context;
+
+  const [activities, setActivity] = useState(null);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const activity = await getActivity(id);
+        setActivity(activity);
+      } catch (error) {
+        throw error;
+      }
+
+    };
+    load();
+  }, [id]);
+
+  async function remove() {
+    if (!activities) return;
+
+    try {
+      const payload = await removeActivity(activity.id);
+      setActivity(null);
+      if (activity) dispatch({ type: 'delete', payload });
+      return payload;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  async function update(change) {
+    if (!activities) return;
+
+    try {
+      const updated = await updateActivity({
+        ...activities,
+        ...change
+      });
+      const payload = {
+        ...updated,
+        name: profile.name
+      };
+      setActivity(payload);
+      if (activity) dispatch({ type: 'update', payload });
+      return payload;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return { activities, remove, update };
 }
