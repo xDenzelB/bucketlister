b37c7e3d15f9976e9adaf8079c223265903953c9
Denzel <dbartolaba123@gmail.com>
2022-05-23 18:36:50 -0700

    added in the routes for activities

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import ViewProfile from "./views/ViewProfile";
 import EditProfile from "./views/EditProfile";
 import Auth from "./views/Auth";
 import { UserProvider } from "./context/UserContext";
+import { BucketListProvider } from "./context/BucketListContext";
+import ViewBucketLists from "./views/ViewBucketLists";
+import AddActivity from "./views/AddActivity";
+import ViewBucketList from "./views/ViewBucketList";
+import EditActivity from "./views/EditActivity";
+import CopyActivities from "./views/CopyActivities";
 
 export default function App() {
   return (
 
     <>
       <UserProvider>
+        <BucketListProvider>
+
         <Router>
           <Switch>
             <Route path='/login'></Route>
@@ -22,10 +30,26 @@ export default function App() {
           </PrivateRoute>
           <PrivateRoute path='/profile'>
             <ViewProfile />
-          </PrivateRoute>
+            </PrivateRoute>
+            <PrivateRoute exact={true} path='/bucketList'>
+              <ViewBucketLists />
+            </PrivateRoute>
+            <PrivateRoute exact={true} path='/bucketList/add'>
+              <AddActivity />
+            </PrivateRoute>
+            <PrivateRoute exact={true} path='/bucketList/:id'>
+              <ViewBucketList />
+            </PrivateRoute>
+            <PrivateRoute exact={true} path='/bucketList/:id/edit'>
+              <EditActivity />
+            </PrivateRoute>
+            <PrivateRoute exact={true} path='/bucketList/:id/copy'>
+              <CopyActivities />
+            </PrivateRoute>
 
 
         </Router>
+        </BucketListProvider>
     </UserProvider>
     </>
   )
